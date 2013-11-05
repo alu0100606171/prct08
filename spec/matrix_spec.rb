1e24bc1b8879dff3aee72e5173ad0f4d99e1758a
Miguel <miguel@mbp.local>
2013-11-05 22:41:48 +0000

    reorganizar

---
+++ spec/matrix_spec.rb
@@ -0,0 +1,53 @@
+require "./lib/matrix_main.rb"
+
+
+describe Matrix do
+  
+  before :each do
+	@f1 = Matrix.new([[1,2],[3,4]])
+  end
+  
+  describe "La matriz se genera correctamente" do
+	it "La matriz tiene formato correcto" do
+	  f = Matrix.new([[1,2],[3]]).should raise_error
+	  f_2 = Matriz.new([[1,2],[3,4]]).should_not raise_error
+	end
+  
+  describe "Almacenamiento de nº de filas y columnas" do
+   it "El numero de filas es correcto" do
+   @f1.filas.should eq(2)
+   end
+   it "El numero de columnas es correcto" do
+   @f1.columnas.should eq(2)
+   end
+  end
+  
+# describe "Almacenamiento de las matrices" do
+#   it "El elemento 0,0 de la matriz se almacena correctamente" do
+#   
+#   end
+#   it "Los elementos de la matriz se almacenan correctamente" do
+#     
+#   end
+# end
+#
+# describe "Operaciones con una matriz" do
+#  it "La traspuesta de la matriz se almacena correctamente" do
+#     
+#   end
+#   
+#   it "La        de la matriz se almacena correctamente" do
+#     
+#   end
+# end
+# 
+# describe "Operaciones con dos matrices" do
+#   it "La suma de matrices se realiza correctamente" do
+#     
+#   end
+#   
+#    it "El producto de matrices se realiza correctamente" do
+#    
+#    end
+#  end
+end
